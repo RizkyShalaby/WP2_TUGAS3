308cea6f3a6dc47b4a228034adec5bb44bd23874
RizkyShalaby <107931863+RizkyShalaby@users.noreply.github.com>
2022-06-21 20:17:23 +0700

    Create v_about.php

---
+++ v_about.php
@@ -0,0 +1,26 @@
+<section>
+	<h1><?php echo $judul ?></h1>
+	<h4>Nama</h4>
+	<ul type="disc">
+		<li>Nama Depan : Mochamad Rizky</li>
+		<li>Nama Belakang : Muhajir Shalaby</li>
+	</ul>
+	<br>
+	<h4>Alamat</h4>
+	<ul type="none">
+		<li> kP.Gandasoli rt.14/05 Babakan Wanayasa</li>
+	</ul>
+	
+	<h4>Tempat Lahir</h4>
+	<ul type="none">
+		<li>Bandung</li>
+	</ul>
+
+	<h4>Olah Raga Favorit</h4>
+	<ul type="square">
+		<li>Bulutangkis</li>
+		<li>Catur</li>
+	</ul>
+	
+
+</section>
